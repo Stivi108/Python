daa6700bf252a0f141d97b065d6c23616ec7cc09
Stivi108 <74671437+Stivi108@users.noreply.github.com>
2020-11-18 21:10:53 +0500

    Add files via upload

---
+++ RomNumConvert.py
@@ -0,0 +1,25 @@
+
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romLib = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        alls = list(s)
+        alnum, i = 0, 0
+        while i in range(len(alls)-1):
+            if romLib[alls[i]] > romLib[alls[i+1]]:
+                alnum += romLib[alls[i]]
+            elif romLib[alls[i]] == romLib[alls[i+1]]:
+                alnum += romLib[alls[i]]
+                if i+2 != len(alls):
+                    if romLib[alls[i]] == romLib[alls[i+2]]:
+                        alnum += romLib[alls[i]]
+            elif romLib[alls[i]] < romLib[alls[i+1]]:
+                alnum += romLib[alls[i+1]] - romLib[alls[i]]
+                i += 1
+            i += 1
+        return alnum
+
+s = 'MCMXCIV'
+transporter = Solution()
+print(transporter.romanToInt(s))
